61f0b5aa3a59a7fd643bf6a65cca7a183f5000eb
Nicholas Krilis <n.krilis@gmail.com>
2022-03-24 02:00:33 -0400

    reated general template for portfolio

---
+++ portfolio/src/components/Copyright.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "../styles/styles.css";
+
+function Copyright() {
+  return (
+    // <!-- Copyright Section-->
+        <div class="copyright py-4 text-center text-white">
+            <div class="container"><small>Copyright &copy; Your Website 2022</small></div>
+        </div>
+  )
+}
+
+export default Copyright;
