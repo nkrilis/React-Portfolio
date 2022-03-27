6743fc009084c18df1d14617d32f2b2426edc73a
Nicholas Krilis <n.krilis@gmail.com>
2022-03-27 18:37:45 -0400

    Just changing up some text

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
     // <!-- Navigation-->
         <nav className="navbar navbar-expand-lg bg-dark text-uppercase fixed-top" id="mainNav">
             <div className="container">
-                <a className="navbar-brand" href="#page-top">Nicholas Krilis</a>
+                <a className="navbar-brand" href="#page-top">Nick Krilis</a>
                 <button className="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i className="fas fa-bars"></i>
